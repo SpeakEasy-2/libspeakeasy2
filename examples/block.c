dce3b66e84e72cf8b66b8089378b1af7551586ee
David R Connell <davidconnell12@gmail.com>
2024-03-28 23:43:16 -0500

    Order adj plot by ground truth
    - Show that SE2 recovered the ground truth community structure

--- examples/block.c
+++ examples/block.c
@@ -11,10 +11,13 @@ int main()
   igraph_real_t type_dist_arr[] = { 0.4, 0.25, 0.2, 0.15 };
   igraph_matrix_t pref_mat;
   igraph_matrix_int_t membership;
+  igraph_vector_int_t ground_truth;
+  igraph_matrix_int_t gt_membership;
   igraph_vector_int_t ordering;
 
   // Generate a graph with clear community structure
   igraph_vector_view(&type_dist, type_dist_arr, n_types);
+  igraph_vector_int_init(&ground_truth, 0);
 
   igraph_matrix_init(&pref_mat, n_types, n_types);
   igraph_real_t p_in = 1 - mu, p_out = mu / (n_types - 1);
@@ -25,7 +28,7 @@ int main()
   }
 
   igraph_preference_game(&graph, n_nodes, n_types, &type_dist, false,
-                         &pref_mat, NULL, IGRAPH_UNDIRECTED, false);
+                         &pref_mat, &ground_truth, IGRAPH_UNDIRECTED, false);
 
   igraph_matrix_destroy(&pref_mat);
 
@@ -37,7 +40,11 @@ int main()
   };
 
   speak_easy_2(&graph, NULL, &opts, &membership);
-  se2_order_nodes(&membership, &ordering);
+
+  // Order nodes by ground truth community structure
+  igraph_matrix_int_view_from_vector(&gt_membership, &ground_truth, 1);
+  se2_order_nodes(&gt_membership, &ordering);
+  igraph_vector_int_destroy(&ground_truth);
 
   // Display results
   puts("Membership");
